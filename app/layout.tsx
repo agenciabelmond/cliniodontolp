c49451720e25b8f00fb8df0529d238217fe90a31
camilatabordac <camilatabordach@gmail.com>
2025-09-01 17:02:30 -0300

    analise

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: 'CliniOdonto',
@@ -20,12 +20,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}
-        <Analytics/>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZB6G6ZGWHJ"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZB6G6ZGWHJ');
+          `}
+        </Script>
+      </head>
+      <body>
+        {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
   )
 }
+
 
 
